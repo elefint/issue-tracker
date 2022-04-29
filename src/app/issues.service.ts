ea9840037c8f74c9c792d11f4493651422ae4757
elefint <mlvngstne@gmail.com>
2022-04-29 11:57:33 +0100

    Initial

---
+++ src/app/issues.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { issues } from '../assets/mock-issues';
+import { Issue } from "./issue";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssuesService {
+
+  private issues: Issue[] = issues;
+
+  constructor() {
+  }
+
+  getPendingIssues(): Issue[] {
+    return this.issues.filter(issue =>
+      !issue.completed);
+  }
+
+  createIssue(issue: Issue) {
+    issue.issueNo = this.issues.length + 1;
+    this.issues.push(issue);
+  }
+
+}
